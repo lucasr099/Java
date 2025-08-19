decde8c42f522d02435a5bfe203fa7e9164658ae
lucasr099 <lucasreiiss100@hotmail.com>
2025-08-19 19:09:09 -0300

    Herança

---
+++ src/javacore/Herança/Teste/HerancaTeste.java
@@ -0,0 +1,26 @@
+package javacore.Herança.Teste;
+
+import javacore.Herança.dominio.Endereco;
+import javacore.Herança.dominio.Funcionario;
+import javacore.Herança.dominio.Pessoa;
+
+public class HerancaTeste {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua do Parque, 40");
+        endereco.setCep("40320465");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Tanramina");
+        pessoa.setCpf("456789844651");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Lucas");
+        funcionario.setCpf("789466684654");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(5465);
+        System.out.println("-----------------------------");
+        funcionario.imprime();
+
+    }
+}
